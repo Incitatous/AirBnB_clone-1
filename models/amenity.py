71290397259702458efedb5cee8bef82ab105283
Jimmy Tran <jimmy.tran@holbertonschool.com>
2017-03-23 22:26:44 +0000

    Testing new version

--- models/amenity.py
+++ models/amenity.py
@@ -1,9 +1,13 @@
 #!/usr/bin/python3
 from models import *
+from sqlalchemy import Column, String
+from models.base_model import BaseModel, Base
 
-
-class Amenity(BaseModel):
+class Amenity(BaseModel, Base):
     name = ""
 
-    def __init__(self, *args, **kwargs):
+    __tablename__ = 'amenities'
+    name = Column(String(128), nullable=False)
+	
+	def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
