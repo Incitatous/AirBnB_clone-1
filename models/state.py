953bfe34161e7a7b4c1f3df4d7747c76b4bf52f2
Jimmy Tran <jimmy.tran@holbertonschool.com>
2017-03-23 22:35:55 +0000

    Test 5

--- models/state.py
+++ models/state.py
@@ -1,9 +1,13 @@
 #!/usr/bin/python3
 from models import *
+from sqlalchemy import Column, String
+from models.base_model import BaseModel, Base
 
-
-class State(BaseModel):
+class State(BaseModel, Base):
     name = ""
+
+    __tablename__ = 'states'
+    name = Column(String(128), nullable=False)
 
     def __init__(self, *args, **kwargs):
         super(State, self).__init__(*args, **kwargs)
