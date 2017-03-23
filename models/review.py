a079387f508016ea7666a628d7a81a1a9e3cdf1f
Jimmy Tran <jimmy.tran@holbertonschool.com>
2017-03-23 22:34:40 +0000

    Test 4

--- models/review.py
+++ models/review.py
@@ -1,11 +1,17 @@
 #!/usr/bin/python3
 from models import *
+from sqlalchemy import Column, String, ForeignKey
+from models.base_model import BaseModel, Base
 
-
-class Review(BaseModel):
+class Review(BaseModel, Base):
     place_id = ""
     user_id = ""
     text = ""
 
-    def __init__(self, *args, **kwargs):
+    __tablename__ = 'reviews'
+    place_id = Column(String(60), nullable=False, ForeignKey("places.id"))
+    user_id = Column(String(60), nullable=False, ForeignKey("users.id")) 
+    text = Column(String(1024), nullable=False)
+	
+	def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
