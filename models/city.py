84025571e22436a77d4ea9bc160ba9781aded846
Jimmy Tran <jimmy.tran@holbertonschool.com>
2017-03-24 03:32:49 +0000

    pep8

--- models/city.py
+++ models/city.py
@@ -3,6 +3,7 @@ from models import *
 from sqlalchemy import Column, String, ForeignKey
 from models.base_model import BaseModel, Base
 
+
 class City(BaseModel, Base):
     state_id = ""
     name = ""
@@ -10,6 +11,6 @@ class City(BaseModel, Base):
     __tablename__ = 'cities'
     name = Column(String(128), nullable=False)
     state_id = Column(String(60), nullable=False, ForeignKey("states.id"))
-    
+
     def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs) 
+        super().__init__(*args, **kwargs)
