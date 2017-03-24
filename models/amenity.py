9f74d9001ca4a3d4c891a40f3b1c58722346df5a
Jimmy Tran <jimmy.tran@holbertonschool.com>
2017-03-24 03:31:14 +0000

    pep8

--- models/amenity.py
+++ models/amenity.py
@@ -3,11 +3,12 @@ from models import *
 from sqlalchemy import Column, String
 from models.base_model import BaseModel, Base
 
+
 class Amenity(BaseModel, Base):
     name = ""
 
     __tablename__ = 'amenities'
     name = Column(String(128), nullable=False)
-	
-	def __init__(self, *args, **kwargs):
+
+    def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
