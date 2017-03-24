e86ab74a0bc11316e1e1633c90cec12c788cabd5
Jimmy Tran <jimmy.tran@holbertonschool.com>
2017-03-24 03:44:33 +0000

    pep8

--- models/user.py
+++ models/user.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 from models import *
 from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship, backref
 from models.base_model import BaseModel, Base
+
 
 class User(BaseModel, Base):
     email = ""
@@ -14,6 +16,8 @@ class User(BaseModel, Base):
     password = Column(String(128), nullable=False)
     first_name = Column(String(128), nullable=False)
     last_name = Column(String(128), nullable=False)
+    place = relationship("Place", backref="user",
+                         cascade="all, delete, delete-orphan")
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
