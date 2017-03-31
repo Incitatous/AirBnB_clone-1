202ef956e4e5be8ce2cc1b13b638e9db8622eb4b
Incitatous <dcianberlin@gmail.com>
2017-03-30 17:11:38 -0700

    pep8

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -14,6 +14,7 @@ def statesList():
     n = storage.all("State")
     return render_template('9-states.html', n=n)
 
+
 @app.route('/states/<id>')
 def statesId(id):
     o = storage.all("State")
@@ -22,7 +23,6 @@ def statesId(id):
         if id == i:
             identity = o[i]
     return render_template('9-states.html', identity=identity)
-        
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
